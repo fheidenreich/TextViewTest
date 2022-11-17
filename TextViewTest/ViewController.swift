65e1c2c856016421e13a2c8486c265fc60e76cd9
Florian Heidenreich <fheidenreich@gmail.com>
2022-11-17 17:07:19 +0100

    initial commit

--- TextViewTest/ViewController.swift
+++ TextViewTest/ViewController.swift
@@ -1,26 +1,25 @@
-//
-//  ViewController.swift
-//  TextViewTest
-//
-//  Created by Florian on 15.11.22.
-//
-
 import Cocoa
 
 class ViewController: NSViewController {
 
+    @IBOutlet weak var scrollView: NSScrollView!
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var textView: NSTextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // This text view doesn't have the slightly grayer control background with behind-window blending that the text field has.
+        textView.drawsBackground = false
+        textView.backgroundColor = .textBackgroundColor
+        textView.textColor = .controlTextColor
+        textView.font = NSFont.systemFont(ofSize: NSFont.systemFontSize)
+
+        scrollView.drawsBackground = false
+        // The focus ring color is also different from the text field focus ring color.
+        scrollView.focusRingType = .exterior
+
+        textView.string = "NSTextView"
+        textField.stringValue = "NSTextField"
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
-    }
-
-
 }
-
